04829e2e3d56e9b4919d368974e8b258c6d7027a
grififth <stevez9101@gmail.com>
2022-02-23 12:32:17 -0500

    Add Enter

--- src/components/InputMenu.js
+++ src/components/InputMenu.js
@@ -7,7 +7,7 @@ const InputMenu = ({ updateWord, submitWord }) => {
     return (
         <div className="inputMenu">
             <div className="inputForm">
-                <input type="text" id="inputField" onChange={(e) => updateWord(e)}></input>
+                <input type="text" id="inputField" onChange={(e) => updateWord(e)} onKeyPress={(e) => {if(e.key === "Enter") submitWord(e)}}></input>
                 <input type="submit" className="button-45" onClick={(e) => submitWord(e)}></input>
             </div>
         </div>
